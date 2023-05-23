ae4521050933548a004d34688f0c15dd986731c1
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-23 21:21:42 +0600

    Shared component cover created and used in menu page

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
 
 const Menu = () => {
   return (
@@ -7,6 +9,7 @@ const Menu = () => {
         <title>Menu - Bistro Boss Restaurant</title>
       </Helmet>
       <h2>This is menu</h2>
+      <Cover img={menuImg} title="our menu"></Cover>
     </div>
   );
 };
