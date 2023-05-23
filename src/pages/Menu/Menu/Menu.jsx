58893a75707d71fdc1ed72aa731ca5abfb8b7161
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-24 00:13:24 +0600

    menu page cover image change

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover";
-import menuImg from "../../../assets/menu/menu-bg.png";
+import menuImg from "../../../assets/menu/banner3.jpg";
 import soupImg from "../../../assets/menu/soup-bg.jpg";
 import saladImg from "../../../assets/menu/salad-bg.jpg";
 import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
